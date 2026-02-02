96970798d2605230d4c4b8c44f879c22711ac10a
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2026-02-02 23:02:07 +0400

    Add AddressContext and AddressService with CRUD operations
    Introduced AddressContext for EF Core Address entity management. Added AddressService providing async CRUD methods for Address, utilizing modern EF Core features and basic error handling.

---
+++ DataFormulaLibrary/Data/AddressContext.cs
@@ -0,0 +1,9 @@
+﻿using DataFormulaLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataFormulaLibrary.Data;
+
+public class AddressContext(DbContextOptions<AddressContext> options) : DbContext(options)
+{
+    public DbSet<Address> Addresses { get; set; }
+}
---
+++ ServiceFormulaLibrary/AddressService.cs
@@ -0,0 +1,44 @@
+﻿using DataFormulaLibrary.Data;
+using DataFormulaLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServiceFormulaLibrary;
+
+public class AddressService(AddressContext context)
+{
+    public async Task<Address?> GetAddressAsync(int addressId)
+    {
+        return await context.Addresses.AsNoTracking()
+            .FirstOrDefaultAsync(d => d.Id == addressId);
+    }
+
+    public async Task<bool> DeleteAddressAsync(int addressId)
+    {
+        try
+        {
+            int affectedRows = await context.Addresses
+                .Where(d => d.Id == addressId)
+                .ExecuteDeleteAsync();
+            return affectedRows > 0 && affectedRows < 2;
+        }
+        catch
+        {
+            //TODO: Add logging
+            return false;
+        }
+    }
+
+    public async Task<Address?> EditAddressAsync(Address Address)
+    {
+        context.Addresses.Update(Address);
+        await context.SaveChangesAsync();
+        return Address;
+    }
+
+    public async Task<int> CreateAddressAsync(Address Address)
+    {
+        context.Addresses.Add(Address);
+        await context.SaveChangesAsync();
+        return Address.Id;
+    }
+}
